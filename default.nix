a558814035443d5b87234f4b084cdac2d636acee
Alex Ameen <alex.ameen.tx@gmail.com>
2023-12-31 18:44:45 -0600

    enable check by default

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, autoreconfHook, jq, bats }: let
+{ stdenv, lib, autoreconfHook, jq, bats, doCheck ? true }: let
   batsWith = bats.withLibraries (p: [
     p.bats-assert
     p.bats-file
@@ -10,5 +10,6 @@ in stdenv.mkDerivation {
   version = "0.1.0";
   nativeBuildInputs = [autoreconfHook batsWith];
   buildInputs = [jq];
+  inherit doCheck;
   meta.license = lib.licenses.gpl3;
 }
